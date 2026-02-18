a4b440d55b779653ba1556e41261e25b6a277a62
AA-STUDI <a.allart@levelup-cluster.eu>
2026-02-18 15:36:11 +0100

    inverted deletion order + non-blocking errors

--- delete.ps1
+++ delete.ps1
@@ -1,16 +1,16 @@
 Write-Host "Suppression en cours..."
 
-kubectl delete -f https://download.elastic.co/downloads/eck/3.3.0/crds.yaml | Out-Null
-if ($LASTEXITCODE -ne 0) { Write-Host "Erreur lors de la suppression CRDs" ; exit 1 }
-kubectl delete -f https://download.elastic.co/downloads/eck/3.3.0/operator.yaml | Out-Null
-if ($LASTEXITCODE -ne 0) { Write-Host "Erreur lors de la suppression Operator ECK" ; exit 1 }
-kubectl delete -f storage-class.yaml | Out-Null
-if ($LASTEXITCODE -ne 0) { Write-Host "Erreur lors de la suppression Storage Class" ; exit 1 }
-kubectl delete -f elasticsearch.yaml | Out-Null
-if ($LASTEXITCODE -ne 0) { Write-Host "Erreur lors de la suppression Elasticsearch" ; exit 1 }
-kubectl delete -f kibana.yaml | Out-Null
-if ($LASTEXITCODE -ne 0) { Write-Host "Erreur lors de la suppression Kibana" ; exit 1 }
 kubectl delete -f filebeat-kubernetes.yaml | Out-Null
-if ($LASTEXITCODE -ne 0) { Write-Host "Erreur lors de la suppression Filebeat" ; exit 1 }
+if ($LASTEXITCODE -ne 0) { Write-Host "Erreur lors de la suppression Filebeat" }
+kubectl delete -f kibana.yaml | Out-Null
+if ($LASTEXITCODE -ne 0) { Write-Host "Erreur lors de la suppression Kibana" }
+kubectl delete -f elasticsearch.yaml | Out-Null
+if ($LASTEXITCODE -ne 0) { Write-Host "Erreur lors de la suppression Elasticsearch" }
+kubectl delete -f storage-class.yaml | Out-Null
+if ($LASTEXITCODE -ne 0) { Write-Host "Erreur lors de la suppression Storage Class" }
+kubectl delete -f https://download.elastic.co/downloads/eck/3.3.0/operator.yaml | Out-Null
+if ($LASTEXITCODE -ne 0) { Write-Host "Erreur lors de la suppression Operator ECK" }
+kubectl delete -f https://download.elastic.co/downloads/eck/3.3.0/crds.yaml | Out-Null
+if ($LASTEXITCODE -ne 0) { Write-Host "Erreur lors de la suppression CRDs" }
 
-Write-Host "...SUCCES !"
+Write-Host "...TERMINE !"
--- delete.sh
+++ delete.sh
@@ -2,17 +2,17 @@
 
 echo "Suppression en cours..."
 
-kubectl delete -f https://download.elastic.co/downloads/eck/3.3.0/crds.yaml
-if [ $? -ne 0 ]; then echo "Erreur lors de la suppression CRDs" ; exit 1 ; fi
-kubectl delete -f https://download.elastic.co/downloads/eck/3.3.0/operator.yaml
-if [ $? -ne 0 ]; then echo "Erreur lors de la suppression Operator ECK" ; exit 1 ; fi
-kubectl delete -f storage-class.yaml
-if [ $? -ne 0 ]; then echo "Erreur lors de la suppression Storage Class" ; exit 1 ; fi
-kubectl delete -f elasticsearch.yaml
-if [ $? -ne 0 ]; then echo "Erreur lors de la suppression Elasticsearch" ; exit 1 ; fi
-kubectl delete -f kibana.yaml
-if [ $? -ne 0 ]; then echo "Erreur lors de la suppression Kibana" ; exit 1 ; fi
 kubectl delete -f filebeat-kubernetes.yaml
-if [ $? -ne 0 ]; then echo "Erreur lors de la suppression Filebeat" ; exit 1 ; fi
+if [ $? -ne 0 ]; then echo "Erreur lors de la suppression Filebeat" ; fi
+kubectl delete -f kibana.yaml
+if [ $? -ne 0 ]; then echo "Erreur lors de la suppression Kibana" ; fi
+kubectl delete -f elasticsearch.yaml
+if [ $? -ne 0 ]; then echo "Erreur lors de la suppression Elasticsearch" ; fi
+kubectl delete -f storage-class.yaml
+if [ $? -ne 0 ]; then echo "Erreur lors de la suppression Storage Class" ; fi
+kubectl delete -f https://download.elastic.co/downloads/eck/3.3.0/operator.yaml
+if [ $? -ne 0 ]; then echo "Erreur lors de la suppression Operator ECK" ; fi
+kubectl delete -f https://download.elastic.co/downloads/eck/3.3.0/crds.yaml
+if [ $? -ne 0 ]; then echo "Erreur lors de la suppression CRDs" ; fi
 
-echo "...SUCCES !"
+echo "...TERMINE !"
